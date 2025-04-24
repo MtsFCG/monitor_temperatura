2e347e97d62bed3fce3d06e005b61a1a9c71792b
Francisco Canales <franciscocanalesgonzalez@live.cl>
2025-04-24 00:23:02 -0400

    build: deteccion de placa en forma automatica

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,37 @@ from flask import Flask, jsonify
 import serial
 import threading
 from flask_cors import CORS
+import glob
+import os
 
 app = Flask(__name__)
 CORS(app, origins=["http://192.168.1.116:8080"])
 
-# Configuración del puerto serial
-SERIAL_PORT = '/dev/ttyUSB0'
+def encontrar_puerto_ch340():
+    # Buscar puertos seriales disponibles
+    for puerto in glob.glob('/dev/ttyUSB*'):
+        # Construir la ruta al directorio del dispositivo en /sys
+        sys_path = f"/sys/class/tty/{puerto.split('/')[-1]}/device"
+        driver_path = f"{sys_path}/driver"
+
+        # Verificar si el dispositivo usa el controlador ch341-uart
+        if os.path.exists(driver_path):
+            # Leer el nombre del controlador
+            driver_name = os.readlink(driver_path).split('/')[-1]
+            if driver_name == "ch341-uart":  # Ajustado para "ch341-uart"
+                return puerto  # Retornar el puerto encontrado
+
+    return None  # Si no se encuentra ningún CH340
+
+# Ejecutar la función
+puerto_arduino = encontrar_puerto_ch340()
+
+if puerto_arduino:
+    print(f"Puerto seleccionado: {puerto_arduino}")
+else:
+    print("No se encontró el dispositivo CH340.")
+
+SERIAL_PORT = puerto_arduino 
 BAUD_RATE = 9600
 
 # Variable global para almacenar el último dato recibido
